0b00e59605b78f7b84bfde1d3d49c5e4ff74ea24
iameo <okwudike.emmanuel@gmail.com>
2022-04-30 23:24:49 +0100

    custom template tag

---
+++ services/templatetags/filter_num.py
@@ -0,0 +1,18 @@
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='filter_num', is_safe=False)
+def filter_num(val, precision=2):
+    try:
+        int_val = int(val)
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            f'Value must be an integer. {val} is not an integer')
+    if int_val < 1000:
+        return str(int_val)
+    elif int_val < 1_000_000:
+        return f'{ int_val/1000.0:.{precision}f}'.rstrip('0').rstrip('.') + 'K'
+    else:
+        return f'{int_val/1_000_000.0:.{precision}f}'.rstrip('0').rstrip('.') + 'M'
