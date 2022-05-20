67d601407e79aa676bf24e04fa904322a099be51
iameo <okwudike.emmanuel@gmail.com>
2022-05-20 20:13:26 +0100

    helper func

---
+++ socialauth/twi_helper.py
@@ -0,0 +1,32 @@
+
+import os
+import twitter
+from rest_framework import serializers
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class TwitterAuthTokenVerification:
+    @staticmethod
+    def validate_access_tokens(access_token_key, access_token_secret):
+        consumer_api_key = os.getenv('CONSUMER_API_KEY')
+        consumer_api_Secret= os.getenv('CONSUMER_API_SECRET')
+
+        try:
+            api = twitter.Api(
+                consumer_key=consumer_api_key,
+                consumer_secret=consumer_api_Secret,
+                access_token_key=access_token_key,
+                access_token_secret=access_token_secret
+            )
+            user_profile = api.VerifyCredentials(include_email=True)
+        except Exception as e:
+            raise serializers.ValidationError({
+                "tokens": ["The tokens are invalid"],
+                "error": str(e)
+            })
+        else:
+            print("55REE: ", access_token_key, access_token_secret, user_profile)
+            return user_profile
