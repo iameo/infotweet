0e12fd13f0bc8aa693d5332fb11b99cbbbaa7623
iameo <okwudike.emmanuel@gmail.com>
2022-05-20 20:19:34 +0100

    services urls dummy

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 
-from .views import home_view, search_tweets
+from .views import home_view, search_tweets, ff
   
 urlpatterns = [
     path('', home_view, name='home-view'),
-    path('search/', search_tweets, name='search_tweets')
+    path('search/', search_tweets, name='search_tweets'),
+    path('abc/', ff, name='ab'),
+    # path('f/', twi_auth, name='auth')
 ]
