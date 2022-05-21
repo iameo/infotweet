49297a3d6fae8ef5760068a8b0aa669dfe14398e
iameo <okwudike.emmanuel@gmail.com>
2022-05-21 09:10:00 +0100

    tets

--- services/urls.py
+++ services/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
 
-from .views import home_view, search_tweets, ff
+from .views import home_view, search_tweets, ff, getClient
   
 urlpatterns = [
     path('', home_view, name='home-view'),
     path('search/', search_tweets, name='search_tweets'),
     path('abc/', ff, name='ab'),
-    # path('f/', twi_auth, name='auth')
+    path('f/', getClient, name='authf')
 ]
