ab0c56610305828705c0ce716ced12ef47c22e73
iameo <okwudike.emmanuel@gmail.com>
2022-05-21 20:38:22 +0100

    cleaned up urls

--- services/urls.py
+++ services/urls.py
@@ -1,11 +1,10 @@
 
 from django.urls import path
 
-from .views import home_view, search_tweets, ff, getClient
+from .views import home_view, search_tweets, ff
   
 urlpatterns = [
     path('', home_view, name='home-view'),
     path('search/', search_tweets, name='search_tweets'),
     path('welcome/', ff, name='welcome'),
-    path('f/', getClient, name='authf')
 ]
